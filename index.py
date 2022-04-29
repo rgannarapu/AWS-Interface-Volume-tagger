945ac781395db6a723c925a7056f9631595c25d6
rgannarapu <rakshith.gannarapu@healthstream.com>
2022-04-29 01:07:20 -0400

    adding volume support

--- index.py
+++ index.py
@@ -27,6 +27,23 @@ def prepare_tags_for_iterface(instance_tags):
 
     return interface_tags
 
+def prepare_tags_for_volume(instance_tags):
+    volume_tags = []
+
+    if instance_tags != None:
+        for each_tag in instance_tags:
+            temp_each_tags = {}
+            if each_tag['Key'].lower() == "name":
+                temp_each_tags['Key'] = each_tag['Key']
+                temp_each_tags['Value'] = each_tag['Value'] + "-volume"
+                volume_tags.append(temp_each_tags)
+            else:
+                temp_each_tags['Key'] = each_tag['Key']
+                temp_each_tags['Value'] = each_tag['Value']
+                volume_tags.append(temp_each_tags)
+
+    return volume_tags
+
 def attach_tags_to_interface(instance,interface_tags):
     for each_interface in instance.network_interfaces:
 
@@ -38,6 +55,14 @@ def attach_tags_to_interface(instance,interface_tags):
             )
             print(response)
 
+def attach_tags_to_volume(instance,volume_tags):
+    for each_volume in instance.volumes.all():
+        if each_volume.tags == None:
+            response = each_volume.create_tags(
+                DryRun=True,
+                Tags=volume_tags
+            )
+
 def ec2_tags_to_interface(instance_list):
     for instance in instance_list:
         interface_tags = prepare_tags_for_iterface(instance.tags)
@@ -46,9 +71,20 @@ def ec2_tags_to_interface(instance_list):
         else:
             print("{instance_id} has to no tags to attach to network interface".format(instance_id = instance.id))
 
+def ec2_tags_to_volume(instance_list):
+    
+    for instance in instance_list:
+        volume_tags = prepare_tags_for_volume(instance.tags)
+        if len(volume_tags) > 0:
+            attach_tags_to_volume(instance,volume_tags)
+        else:
+            print("{instance_id} has to no tags to attach to volumes".format(instance_id = instance.id))
+
+
 def main():
     instance_list = list_instance()
-    ec2_tags_to_interface(instance_list)
+    # ec2_tags_to_interface(instance_list)
+    ec2_tags_to_volume(instance_list)
 
 if __name__ == "__main__":
     main()
